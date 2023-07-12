fe5552d986be3cba84238e94d2c3fbd03afccf9a
Pedro Yvo <62101651+pmartins1301@users.noreply.github.com>
2023-07-12 17:36:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Msg(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+    return {"message": "Teste. Welcome to FastAPI!"}
 
 
 @app.get("/path")
